35a174fa38dd249c957963d30843fd113ebc97ac
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2026-03-16 01:26:09 +0530

    Step logging into test report added

--- src/test/java/com/saddy/tests/LoginTest.java
+++ src/test/java/com/saddy/tests/LoginTest.java
@@ -1,6 +1,8 @@
 package com.saddy.tests;
 
 import com.saddy.pages.LoginPage;
+import com.seleniumboot.steps.StepLogger;
+import com.seleniumboot.steps.StepStatus;
 import com.seleniumboot.test.BaseTest;
 import org.testng.annotations.Test;
 
@@ -8,10 +10,14 @@ public class LoginTest extends BaseTest {
     @Test(description = "Valid login with correct credentials")
     public void loginTest(){
         open();
+        StepLogger.step("Navigate to Login Page");
         LoginPage loginPage = new LoginPage();
 
         loginPage.enterUsername("admin");
         loginPage.enterPassword("password");
+        StepLogger.step("Entered username and password", true);
+
         loginPage.clickLoginButton();
+        StepLogger.step("Clicked Login button", StepStatus.PASS);
     }
 }
